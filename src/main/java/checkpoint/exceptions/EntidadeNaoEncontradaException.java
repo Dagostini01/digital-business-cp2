3ef8a8f65d9e28631d84363c66f30fe4338f9eb2
SassaTricolado <contatos.samuel@outlook.com>
2023-04-21 17:21:54 -0300

    ProductService

---
+++ src/main/java/checkpoint/exceptions/EntidadeNaoEncontradaException.java
@@ -0,0 +1,9 @@
+package checkpoint.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+	public EntidadeNaoEncontradaException(String mensagem) {
+		super(mensagem);
+	}
+
+}
--- src/main/java/checkpoint/services/ProdutoService.java
+++ src/main/java/checkpoint/services/ProdutoService.java
@@ -2,10 +2,12 @@ package checkpoint.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import checkpoint.entities.ProdutoEntity;
+import checkpoint.exception.EntidadeNaoEncontradaException;
 import checkpoint.repositories.ProdutoRepository;
 
 @Service
@@ -16,6 +18,24 @@ public class ProdutoService {
 
 	public List<ProdutoEntity> getProduts() {
 		return produtoRepository.findAll();
+	}
+	
+	public ProdutoEntity save(ProdutoEntity produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	public ProdutoEntity search(Long id) {
+		return produtoRepository.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException("Produto não encontrado"));
+	}
+	
+	public ProdutoEntity update(Long id, ProdutoEntity produto) {
+		ProdutoEntity produtoEx = search(id);
+		BeanUtils.copyProperties(produto, produtoEx, "id");
+		return produtoRepository.save(produtoEx);
+	}
+	
+	public void deletar(Long id) {
+		produtoRepository.delete(search(id));
 	}
 
 }
