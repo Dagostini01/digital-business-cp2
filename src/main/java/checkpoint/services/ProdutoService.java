0a582059b315881b48cbf95f19ad75292e877815
matheus <matheusnagot@hotmail.com>
2023-04-21 13:36:47 -0300

    feat: add services Usuario & Produto

---
+++ src/main/java/checkpoint/services/ProdutoService.java
@@ -0,0 +1,21 @@
+package checkpoint.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import checkpoint.entities.ProdutoEntity;
+import checkpoint.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public List<ProdutoEntity> getProduts() {
+		return produtoRepository.findAll();
+	}
+
+}
