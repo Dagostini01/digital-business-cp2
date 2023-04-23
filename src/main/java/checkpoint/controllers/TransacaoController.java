89f190622e0c835b46092d93b62f8d77867c4f38
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-04-23 15:18:50 -0300

    pix function

--- src/main/java/checkpoint/controllers/TransacaoController.java
+++ src/main/java/checkpoint/controllers/TransacaoController.java
@@ -32,40 +32,40 @@ public class TransacaoController {
 	@Service
 	public class PixService {
 
-		@PostMapping("/pix")
-		public TransacaoEntity fazerPix(@RequestParam Long idDebitor, @RequestParam String chavePix, @RequestParam Double valor) {
-		    // Verifica se ambos os usuários estão ativos na base de dados
-		    Optional<UsuarioEntity> debitor = usrService.findById(idDebitor);
-		    UsuarioEntity creditor = (chavePix);
-		    if (!debitor.isAtivo() || !creditor.isAtivo()) {
-		        throw new RuntimeException("Usuário não encontrado ou inativo na base de dados");
-		    }
-		    
-		    // Verifica se o saldo do usuário de débito é suficiente para a transação
-		    if (debitor.getSaldo() < valor) {
-		        throw new RuntimeException("Saldo insuficiente");
-		    }
-		    
-		    // Verifica se a chave PIX fornecida pelo usuário existe na base de dados
-		    PixEntity pix = pixService.getPixByValor(chavePix);
-		    if (pix == null) {
-		        throw new RuntimeException("Chave PIX não encontrada na base de dados");
-		    }
-		    
-		    // Subtrai o valor da transação do saldo do usuário de débito
-		    debitor.setSaldo(debitor.getSaldo() - valor);
-		    UsuarioService.salvarUsuario(debitor);
-		    
-		    // Adiciona o valor da transação ao saldo do usuário que possui a chave PIX
-		    creditor.setSaldo(creditor.getSaldo() + valor);
-		    UsuarioService.salvarUsuario(creditor);
-		    
-		    // Cria uma nova transação com os detalhes fornecidos
-		    TransacaoEntity transacao = new TransacaoEntity();
-		    transacao.setStatus(StatusTransacao.SUCESSO);
-		    transacao.setHoraTranscao(Calendar.getInstance());
-		    transacao.setDebitor(debitor);
-		    transacao.setCh
+		//@PostMapping("/pix")
+		//public TransacaoEntity fazerPix(@RequestParam Long idDebitor, @RequestParam String chavePix, @RequestParam Double valor) {
+//		    // Verifica se ambos os usuários estão ativos na base de dados
+//		    Optional<UsuarioEntity> debitor = usrService.findById(idDebitor);
+//		    UsuarioEntity creditor = (chavePix);
+//		    if (!debitor.isAtivo() || !creditor.isAtivo()) {
+//		        throw new RuntimeException("Usuário não encontrado ou inativo na base de dados");
+//		    }
+//		    
+//		    // Verifica se o saldo do usuário de débito é suficiente para a transação
+//		    if (debitor.getSaldo() < valor) {
+//		        throw new RuntimeException("Saldo insuficiente");
+//		    }
+//		    
+//		    // Verifica se a chave PIX fornecida pelo usuário existe na base de dados
+//		    PixEntity pix = pixService.getPixByValor(chavePix);
+//		    if (pix == null) {
+//		        throw new RuntimeException("Chave PIX não encontrada na base de dados");
+//		    }
+//		    
+//		    // Subtrai o valor da transação do saldo do usuário de débito
+//		    debitor.setSaldo(debitor.getSaldo() - valor);
+//		    UsuarioService.salvarUsuario(debitor);
+//		    
+//		    // Adiciona o valor da transação ao saldo do usuário que possui a chave PIX
+//		    creditor.setSaldo(creditor.getSaldo() + valor);
+//		    UsuarioService.salvarUsuario(creditor);
+//		    
+//		    // Cria uma nova transação com os detalhes fornecidos
+//		    TransacaoEntity transacao = new TransacaoEntity();
+//		    transacao.setStatus(StatusTransacao.SUCESSO);
+//		    transacao.setHoraTranscao(Calendar.getInstance());
+//		    transacao.setDebitor(debitor);
+//		    transacao.setCh
 
 
-}
+}}
