b7de357cd2d8aa17e32053e65543e0d95d89922b
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-04-23 15:09:12 -0300

    pix function

--- src/main/java/checkpoint/controllers/TransacaoController.java
+++ src/main/java/checkpoint/controllers/TransacaoController.java
@@ -1,57 +1,71 @@
 package checkpoint.controllers;
 
+import java.util.Calendar;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import checkpoint.entities.PixEntity;
+import checkpoint.entities.TransacaoEntity;
 import checkpoint.entities.UsuarioEntity;
+import checkpoint.enums.StatusTransacao;
 import checkpoint.enums.TipoChave;
 import checkpoint.repositories.PixRepository;
 import checkpoint.repositories.UsuarioRepository;
+import checkpoint.services.UsuarioService;
 
 @RestController
 @RequestMapping("/api/v1/transacao")
 public class TransacaoController {
+	
+	@Autowired
+	private UsuarioService usrService;
+
 	@Service
 	public class PixService {
 
-	    @Autowired
-	    private UsuarioRepository userRepository;
+		@PostMapping("/pix")
+		public TransacaoEntity fazerPix(@RequestParam Long idDebitor, @RequestParam String chavePix, @RequestParam Double valor) {
+		    // Verifica se ambos os usuários estão ativos na base de dados
+		    Optional<UsuarioEntity> debitor = usrService.findById(idDebitor);
+		    UsuarioEntity creditor = (chavePix);
+		    if (!debitor.isAtivo() || !creditor.isAtivo()) {
+		        throw new RuntimeException("Usuário não encontrado ou inativo na base de dados");
+		    }
+		    
+		    // Verifica se o saldo do usuário de débito é suficiente para a transação
+		    if (debitor.getSaldo() < valor) {
+		        throw new RuntimeException("Saldo insuficiente");
+		    }
+		    
+		    // Verifica se a chave PIX fornecida pelo usuário existe na base de dados
+		    PixEntity pix = pixService.getPixByValor(chavePix);
+		    if (pix == null) {
+		        throw new RuntimeException("Chave PIX não encontrada na base de dados");
+		    }
+		    
+		    // Subtrai o valor da transação do saldo do usuário de débito
+		    debitor.setSaldo(debitor.getSaldo() - valor);
+		    UsuarioService.salvarUsuario(debitor);
+		    
+		    // Adiciona o valor da transação ao saldo do usuário que possui a chave PIX
+		    creditor.setSaldo(creditor.getSaldo() + valor);
+		    UsuarioService.salvarUsuario(creditor);
+		    
+		    // Cria uma nova transação com os detalhes fornecidos
+		    TransacaoEntity transacao = new TransacaoEntity();
+		    transacao.setStatus(StatusTransacao.SUCESSO);
+		    transacao.setHoraTranscao(Calendar.getInstance());
+		    transacao.setDebitor(debitor);
+		    transacao.setCh
 
-//	    @Autowired
-//	    private PixRepository pixRepository;
-//
-//	    public ResponseEntity<PixEntity>enviarPix(@RequestBody PixEntity chavePix, @PathVariable Long id) {
-//	        // Busca o usuário remetente pelo id
-//	    	UsuarioEntity buscaUser= 
-//	        //.orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
-//
-//	        // Verifica se o usuário remetente tem saldo suficiente para enviar o Pix
-//	        if (remetente.getSaldo() < valor) {
-//	            throw new RuntimeException("Saldo insuficiente");
-//	        }
-
-	        // Busca o usuário destinatário pelo Pix
-	        PixEntity pixDestinatario = PixRepository.findByValorAndTipo(chavePixDestinatario, TipoChave.CPF).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
-
-	        // Verifica se o usuário destinatário está ativo
-	        if (!pixDestinatario.getUsuario().isAtivo()) {
-	            throw new RuntimeException("Usuário destinatário inativo");
-	        }
-
-	        // Subtrai o valor do remetente e adiciona ao destinatário
-	        remetente.setSaldo(remetente.getSaldo() - valor);
-	        pixDestinatario.getUsuario().setSaldo(pixDestinatario.getUsuario().getSaldo() + valor);
-
-	        // Salva as alterações nos usuários
-	        userRepository.save(remetente);
-	        userRepository.save(pixDestinatario.getUsuario());
-	    }
-	}
 
 }
