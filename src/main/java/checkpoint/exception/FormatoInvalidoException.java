f2faeee3f52a950899545ca6f326fa00ed979de1
matheus <matheusnagot@hotmail.com>
2023-04-22 23:30:19 -0300

    feat: create  exceptions

---
+++ src/main/java/checkpoint/exception/FormatoInvalidoException.java
@@ -0,0 +1,9 @@
+package checkpoint.exception;
+
+public class FormatoInvalidoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public FormatoInvalidoException(String message) {
+		super(message);
+	}
+}
